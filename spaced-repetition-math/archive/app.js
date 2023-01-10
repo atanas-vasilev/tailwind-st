394adf76146ac0cca4e5d516afb14a5b9216f092
Atanas Vasilev <39429743+atanas-vasilev@users.noreply.github.com>
2023-01-10 22:57:55 +0200

    Delete spaced-repetition-math directory

--- spaced-repetition-math/archive/app.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("a.ham-icon").addEventListener("click", function(event){
-    document.getElementById("sideNavigation").style.width = "95vw";
-    document.getElementById("main").style.marginLeft = "95vw";
-  });
-  
-  document.querySelector("a.close-btn").addEventListener("click", function(event){
-    document.getElementById("sideNavigation").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  });
--- spaced-repetition-math/archive/script.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.getElementById('menu-button');
-const nav = document.getElementById('sidebar');
-
-menuButton.addEventListener('click', () => {
-  nav.classList.toggle('hidden');
-});
